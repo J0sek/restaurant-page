0f5c7efbd221af2c5efb442841e069fcb497f4a7
Jose Kim <barsamic03@gmail.com>
2023-02-25 21:37:00 +0900

    Add style.css to src and import it to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const content = document.getElementById("content");
+import { creator } from "./elementCreator.js";
+import "./style.css";
+
+const banner = creator("div", "banner");
+
+const homeBtn = creator("div", "banner-home");
+homeBtn.textContent = "HOME";
+
+const menuBtn = creator("div", "banner-menu");
+menuBtn.textContent = "MENU";
+
+const aboutBtn = creator("div", "banner-about");
+aboutBtn.textContent = "ABOUT";
+
+content.append(banner);
+
+banner.append(homeBtn, menuBtn, aboutBtn);
