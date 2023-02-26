0efc126ca2d42820c32f87b6551fa1130fb07af4
Jose Kim <barsamic03@gmail.com>
2023-02-26 15:22:53 +0900

    Add main tag in index.js for main content

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const content = document.getElementById("content");
 let bannerDOM = banner();
 
 content.append(bannerDOM);
+
+const main = document.createElement("div");
+
+main.classList.add("main", "home");
+
+content.append(main);
