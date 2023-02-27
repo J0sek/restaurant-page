ab1773d75a6973053cb2781f7a86d06af6aec787
Jose Kim <barsamic03@gmail.com>
2023-02-27 20:48:01 +0900

    Add logic inside banner module for switching tab content

--- src/banner.js
+++ src/banner.js
@@ -1,11 +1,30 @@
 import creator from "./elementCreator.js";
-import main from "./mainHome.js";
+import home from "./mainHome.js";
+
+let currentTab = "menu";
+
+function loadTabContent(tab, tabContent) {
+  tab.addEventListener("click", () => {
+    if (tab.classList[1] === currentTab) return;
+
+    currentTab = tab.classList[1];
+
+    let main = document.querySelector(".main");
+
+    let content = document.getElementById("content");
+
+    content.removeChild(main);
+
+    content.append(tabContent);
+  });
+}
 
 function bannerMaker() {
   const banner = creator("div", "banner");
 
   const homeBtn = creator("div", "banner-btn", "home");
   homeBtn.textContent = "HOME";
+  loadTabContent(homeBtn, home);
 
   const menuBtn = creator("div", "banner-btn", "menu");
   menuBtn.textContent = "MENU";
@@ -19,5 +38,9 @@ function bannerMaker() {
 }
 
 let banner = bannerMaker();
+
+let testList = document.querySelectorAll(".banner-btn");
+
+testList.forEach((x) => x.addEventListener("click", () => console.log(1)));
 
 export default banner;
